e4411f918e3dbdc0e6d73f4f338eec392ba1e2a6
CurtisMcKinney <casiosk1@gmail.com>
2013-04-27 19:03:57 -0400

    added cmd+enter for executing code

--- editors/ide/src/core/EditBuffer.cpp
+++ editors/ide/src/core/EditBuffer.cpp
@@ -21,7 +21,11 @@ EditBuffer::EditBuffer(QWidget* parent) :
 
 void EditBuffer::keyPressEvent(QKeyEvent *e)
 {
-    if((e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) && e->modifiers() == Qt::ShiftModifier)
+    if(
+            ((e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) && e->modifiers() == Qt::ShiftModifier)
+            ||
+            ((e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) && e->modifiers() == Qt::ControlModifier)
+        )
     {
         QString command;
         std::string returnString;
