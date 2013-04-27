37d314bf35afdba3513448a2caaf8be5e5f23755
CurtisMcKinney <casiosk1@gmail.com>
2013-04-27 18:28:31 -0400

    removed n

--- src/main.cpp
+++ src/main.cpp
@@ -34,10 +34,10 @@ int main(int argc,char** argv)
     //This is how you read a string into the parser manually
     std::string output;
 
-    panopticon::exec("3*5",output);
+    panopticon::exec("3*5\n",output);
     std::cout << output;
 
-    optic::exec("5.1/10^2",output);
+    optic::exec("5.1/10^2\n",output);
     std::cout << output;
 }
 
