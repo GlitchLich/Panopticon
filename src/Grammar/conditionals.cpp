5a451bd183bd55960f8250daac8f99cba8de25de
Chad McKinney <seppukuzombie@gmail.com>
2013-05-09 17:30:16 +0100

    Working on fix for guard expansion

--- src/Grammar/conditionals.cpp
+++ src/Grammar/conditionals.cpp
@@ -7,6 +7,11 @@
 namespace panopticon
 {
 
+bool evaluate_guard(object& A, const object& boolean, const object& true_func)
+{
+
+}
+
 /**
  * @brief resolve_guard
  * @param A The result. Either an operation tree if one condition is true, or void.
@@ -36,6 +41,10 @@ bool resolve_guard(object& A, const object &condition_tree)
     }
     else if(result.type==FAILED_CONDITION)
     {
+        object bool_func;
+        bool_func.type = OPERATION;
+        bool_func.data.operator_func = evaluate_guard;
+
         for(int i=1;i<condition_tree.data.array->size();++i)
         {
             optic_stack.push_back(condition_tree.data.array->at(i));
